ed724ac9d520295b7964312e1c0d265182e9cade
FukushimaTakeshi <whippet.818@gmail.com>
2018-11-22 18:04:27 +0900

    webpack settings

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+// see https://qiita.com/taaatk/items/3137659e69b82d10cfca
+const path = require('path')
+module.exports = Object.assign({}, environment.toWebpackConfig(), {
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+})
